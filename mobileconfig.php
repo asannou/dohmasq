71dbfc636a25f9f66066e8b8b53c4053c6aa4b53
asannou <hataori@gmail.com>
2025-12-01 22:18:24 +0900

    Refactor: Rewrite mobileconfig.php using DOMDocument
    Rewrite the generation of the Apple configuration profile to use DOMDocument instead of a HEREDOC string.
    This makes the XML generation more robust and maintainable.
    The token used to generate the ServerURL is now dynamically retrieved from the parameters, allowing for more flexible profile generation.

--- mobileconfig.php
+++ mobileconfig.php
@@ -11,50 +11,61 @@ $server_url = 'https://' . $_SERVER['HTTP_HOST'] . '/' . $token . '/dns-query.ph
 $payload_uuid = 'F222B776-4E47-4F3C-8806-B276E4BB79F8';
 $dns_payload_uuid = '8E9F2A0C-2E9E-435A-B903-38062FC6F39B';
 
-echo <<<EOT
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>PayloadContent</key>
-    <array>
-        <dict>
-            <key>PayloadDescription</key>
-            <string>DNS Settings</string>
-            <key>PayloadDisplayName</key>
-            <string>Dohmasq</string>
-            <key>PayloadIdentifier</key>
-            <string>com.apple.dnsSettings.managed.$dns_payload_uuid</string>
-            <key>PayloadType</key>
-            <string>com.apple.dnsSettings.managed</string>
-            <key>PayloadUUID</key>
-            <string>$dns_payload_uuid</string>
-            <key>PayloadVersion</key>
-            <integer>1</integer>
-            <key>DNSSettings</key>
-            <dict>
-                <key>DNSProtocol</key>
-                <string>HTTPS</string>
-                <key>ServerURL</key>
-                <string>$server_url</string>
-                <key>ProhibitDisablement</key>
-                <false/>
-            </dict>
-        </dict>
-    </array>
-    <key>PayloadDisplayName</key>
-    <string>Dohmasq</string>
-    <key>PayloadIdentifier</key>
-    <string>com.apple.dnsSettings.managed</string>
-    <key>PayloadRemovalDisallowed</key>
-    <false/>
-    <key>PayloadType</key>
-    <string>Configuration</string>
-    <key>PayloadUUID</key>
-    <string>$payload_uuid</string>
-    <key>PayloadVersion</key>
-    <integer>1</integer>
-</dict>
-</plist>
-EOT;
+$doc = new DOMDocument('1.0', 'UTF-8');
+$doc->formatOutput = true;
+
+$doctype = $doc->implementation->createDocumentType('plist', '-//Apple//DTD PLIST 1.0//EN', 'http://www.apple.com/DTDs/PropertyList-1.0.dtd');
+$doc->appendChild($doctype);
+
+$plist = $doc->createElement('plist');
+$plist->setAttribute('version', '1.0');
+$doc->appendChild($plist);
+
+$rootDict = $doc->createElement('dict');
+$plist->appendChild($rootDict);
+
+function addKeyValuePair(DOMDocument $doc, DOMElement $dict, string $key, string $type, string|int|bool $value): void
+{
+    $keyElement = $doc->createElement('key', htmlspecialchars($key));
+    $dict->appendChild($keyElement);
+
+    if ($type === 'string' || $type === 'integer') {
+        $valueElement = $doc->createElement($type, htmlspecialchars((string)$value));
+        $dict->appendChild($valueElement);
+    } elseif ($type === 'true' || $type === 'false') {
+        $valueElement = $doc->createElement($type);
+        $dict->appendChild($valueElement);
+    }
+}
+
+$rootDict->appendChild($doc->createElement('key', 'PayloadContent'));
+$payloadContentArray = $doc->createElement('array');
+$rootDict->appendChild($payloadContentArray);
+
+$dnsPayloadDict = $doc->createElement('dict');
+$payloadContentArray->appendChild($dnsPayloadDict);
+
+addKeyValuePair($doc, $dnsPayloadDict, 'PayloadDescription', 'string', 'DNS Settings');
+addKeyValuePair($doc, $dnsPayloadDict, 'PayloadDisplayName', 'string', 'Dohmasq');
+addKeyValuePair($doc, $dnsPayloadDict, 'PayloadIdentifier', 'string', 'com.apple.dnsSettings.managed.' . $dns_payload_uuid);
+addKeyValuePair($doc, $dnsPayloadDict, 'PayloadType', 'string', 'com.apple.dnsSettings.managed');
+addKeyValuePair($doc, $dnsPayloadDict, 'PayloadUUID', 'string', 'com.apple.dnsSettings.managed');
+addKeyValuePair($doc, $dnsPayloadDict, 'PayloadVersion', 'integer', 1);
+
+$dnsPayloadDict->appendChild($doc->createElement('key', 'DNSSettings'));
+$dnsSettingsDict = $doc->createElement('dict');
+$dnsPayloadDict->appendChild($dnsSettingsDict);
+
+addKeyValuePair($doc, $dnsSettingsDict, 'DNSProtocol', 'string', 'HTTPS');
+addKeyValuePair($doc, $dnsSettingsDict, 'ServerURL', 'string', $server_url);
+addKeyValuePair($doc, $dnsSettingsDict, 'ProhibitDisablement', 'false', false);
+
+addKeyValuePair($doc, $rootDict, 'PayloadDisplayName', 'string', 'Dohmasq');
+addKeyValuePair($doc, $rootDict, 'PayloadIdentifier', 'string', 'com.apple.dnsSettings.managed');
+addKeyValuePair($doc, $rootDict, 'PayloadRemovalDisallowed', 'false', false);
+addKeyValuePair($doc, $rootDict, 'PayloadType', 'string', 'Configuration');
+addKeyValuePair($doc, $rootDict, 'PayloadUUID', 'string', $payload_uuid);
+addKeyValuePair($doc, $rootDict, 'PayloadVersion', 'integer', 1);
+
+echo $doc->saveXML();
 
